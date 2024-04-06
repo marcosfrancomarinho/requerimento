dc364dad0449dac867a6f88b8304a9f3c976f4e9
Marcos Franco Marinho <marcosmarinho19998@gmail.com>
2024-04-06 20:30:51 -0300

    start

---
+++ src/js/app.js
@@ -0,0 +1,20 @@
+const $ = selector => {
+    const element = document.querySelectorAll(selector)
+    return element.length > 1 ? element : element[0]
+}
+$("form").onsubmit = function (event) {
+    event.preventDefault()
+    const formData = Object.fromEntries(new FormData(this))
+    createRequirement.bind(formData)()
+}
+$("#marital_status").onchange = function () {
+    const response = ["casado", "convivente"].some(status => {
+        return status === this.value
+    })
+    $("#spouse").classList.toggle("hide", !response)
+}
+function createRequirement() {
+    $("#form-datas").innerHTML = `
+        <div>Nome: ${this.name}</div>
+    `
+}

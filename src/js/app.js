f97e24347928ef33fd4f72c89a560f13f0c51bb0
Marcos Franco Marinho <marcosmarinho19998@gmail.com>
2024-04-06 23:51:52 -0300

    start

--- src/js/app.js
+++ src/js/app.js
@@ -19,3 +19,35 @@ function createRequirement() {
         <div>Nome: ${this.name}</div>
     `
 }
+async function api_1(cep) {
+    const url_1 = `https://viacep.com.br/ws/${cep}/json/`
+    return await fetch(url_1)
+
+}
+async function api_2(cep) {
+    const url_2 = `https://brasilapi.com.br/api/cep/v1/${cep}`
+    return await fetch(url_2)
+}
+async function apiCep(cep) {
+    const response = await Promise.race([api_1(cep), api_2(cep)])
+    if (response.status === 200) {
+        return response.json()
+    }
+}
+$("#cep").onblur = async function () {
+    const number = Number(this.value)
+    const {
+        logradouro, bairro, uf,
+        localidade, state, city,
+        neighborhood, street
+    } = await apiCep(number)
+    const info = [
+        [logradouro, street],
+        [bairro, neighborhood],
+        [localidade, city],
+        [uf, state]
+    ]
+    $(".automatic_filling").forEach((elment, idx) => {
+        elment.value = info[idx][0] ?? info[idx][1]
+    })
+}

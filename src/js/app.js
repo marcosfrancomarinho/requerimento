5d38af3f5b59defa0307a1c49b826584dcb8607b
Marcos Franco Marinho <marcosmarinho19998@gmail.com>
2024-04-10 20:35:01 -0300

    modelando a page do pdf

--- src/js/app.js
+++ src/js/app.js
@@ -2,10 +2,23 @@ const $ = selector => {
     const element = document.querySelectorAll(selector)
     return element.length > 1 ? element : element[0]
 }
+const separator = (string) => {
+    if (string.length <= 12) {
+        return string.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, '$1.$2.$3-$4');
+    }
+    return string.replace(/(\d{2})(\d{3})(\d{3})(\d{4})(\d{2})/, '$1.$2.$3/$4-$5');
+}
+const mTel = (tel) => {
+    return tel
+        .replace(/(.{0})(\d)/, "$1($2")
+        .replace(/(.{3})(\d)/, "$1)$2")
+        .replace(/(.{4})$/, "-$1")
+}
 $("form").onsubmit = function (event) {
     event.preventDefault()
     const formData = Object.fromEntries(new FormData(this))
     createForm.bind(formData)()
+    pdfGenerator()
 }
 $("#status").onchange = function () {
     const response = ["casado", "convivente"].some(status => {
@@ -49,6 +62,29 @@ $("#cep").onblur = async function () {
 function createForm() {
     Object.keys(this).forEach(key => {
         const value = $("#" + key).value
-        $("#data_" + key).innerText = key !== "email" ? value.toUpperCase() : value.toLowerCase()
+        $("#data_" + key).innerText = value
     })
+}
+$("#cpf").onblur = function () {
+    this.type = "text"
+    this.value = separator(this.value)
+}
+$("#phone").onblur = function () {
+    this.value = mTel(this.value)
+}
+$("#cpf").onclick = function () {
+    this.type = "number"
+    this.value = ""
+}
+$("#phone").onclick = function () {
+    this.value = ""
+}
+function pdfGenerator() {
+    const opt = {
+        margin: 1,
+        filename: 'myfile.pdf',
+        html2canvas: { scale: 1 },
+        jsPDF: { orientation: 'portrait' }
+    };
+    html2pdf().set(opt).from($("#form_data")).save()
 }
